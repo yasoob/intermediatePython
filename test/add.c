48174a5c500efc6e06ed2627dd002bffbfb89eab
Philipp <zhanghongliang@192.168.1.104>
2020-08-21 22:00:47 +0800

    c

---
+++ test/add.c
@@ -0,0 +1,12 @@
+//sample C file to add 2 numbers - int and floats
+
+int add_int(int, int);
+float add_float(float, float);
+
+int add_int(int num1, int num2){
+    return num1 + num2;
+}
+
+float add_float(float num1, float num2){
+    return num1 + num2;
+}
---
+++ test/open.py
@@ -0,0 +1,9 @@
+
+import unittest
+class TDD_OPEN(unittest.TestCase):
+    def test_open(self):
+
+if __name__ == '__main__':
+    unittest.main()
+
+                
